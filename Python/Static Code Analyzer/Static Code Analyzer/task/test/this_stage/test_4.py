9d3105f9e621bd0dde3560d851996367d5cd1931
Berk0f <suslovas88@gmail.com>
2022-02-22 19:09:30 +0400

    Delete Python directory

--- Python/Static Code Analyzer/Static Code Analyzer/task/test/this_stage/test_4.py
+++
@@ -1,9 +0,0 @@
-class Test:
-    def __init__(self, alpha, Beta=1):
-        self.alpha = alpha
-
-    def function(self):
-        print(self.alpha)
-
-    def okey(self, s=None):
-        print("okey")
--- Python/Static Code Analyzer/Topics/ast module/A list of nodes/main.py
+++
@@ -1,9 +0,0 @@
-import ast
-
-expression = "(34 + 6) * (23**2 - 7 + 45**2)"
-
-tree = ast.parse(expression)
-
-print(len([node for node in ast.walk(tree)]))
-
-# put your code here
